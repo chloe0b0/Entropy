e17ef6d33220ce9a2a203552ad9baf7a74d7b07f
chloe0b0 <chloezeiter@gmail.com>
2022-03-09 18:53:10 -0800

    Updated structure

---
+++ src/Rand/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+
+#include "MT19937.h"
+
+int main(void){
+    MT19937 mt;
+    seedMT(&mt, time(NULL));
+
+    for (int i = 0; i < 10; ++i){
+        printf("%lu ", genMTNat(&mt));
+    }
+    
+}
